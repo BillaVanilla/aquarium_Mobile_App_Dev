9afde69384e22cb4bb537e2630609a04689f3302
BillaVanilla <bfofung@gmail.com>
2024-10-23 18:42:45 -0400

    add class fish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,33 +26,29 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class Fish {
+  Color color;
+  double speed;
+
+  Fish({required this.color, required this.speed});
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
 
+  
   @override
   Widget build(BuildContext context) {
       return Scaffold(
       appBar: AppBar(
         
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
       ),
       body: Center(
         
